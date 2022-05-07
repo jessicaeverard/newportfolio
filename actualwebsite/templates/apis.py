94a6c3a625a0d6df4b1963ebc7a0c9367df7eaf6
Jess <jessica@mediatrader.com>
2022-05-07 18:15:18 +0100

    trying to implement apis

---
+++ actualwebsite/templates/apis.py
@@ -0,0 +1,22 @@
+from django import template
+register = template.Library()
+
+import requests
+
+@register.simple_tag
+def random_joke():
+    url = "https://v2.jokeapi.dev/joke/Programming?blacklistFlags=racist,sexist,explicit"
+
+    payload={}
+    headers = {}
+
+    response = requests.request("GET", url, headers=headers, data=payload).json()
+
+    if response['type'] == 'twopart':
+        setup = (response['setup'])
+        delivery = (response['delivery'])
+        return (setup, delivery)
+    else:
+        joke = (response['joke'])
+        return joke
+
