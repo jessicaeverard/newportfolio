a54226160228fcba6eb66ef2854f0c37915c65bb
Jess <jessica@mediatrader.com>
2023-04-16 10:58:54 +0100

    unclean version

--- actualwebsite/models.py
+++ actualwebsite/models.py
@@ -7,3 +7,14 @@ class Contact(models.Model):
     
     def __str__(self):
         return self.name
+    
+class Projects(models.Model):
+    image = models.ImageField(upload_to='projects/images/')
+    title = models.CharField(max_length=50)    
+    description = models.CharField(max_length=500)    
+    link_to_code = models.URLField(blank=True)
+    link_to_live_app = models.URLField(blank=True) 
+
+    def __str__(self):
+        return self.title
+
--- actualwebsite/views.py
+++ actualwebsite/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
 from .forms import ContactForm
+from .models import Projects
 
 def index(request):
     if request.method == 'POST': #method is post as we are inputting data
         form = ContactForm(request.POST) #creating an instance for this form
         newContact = form.save(commit=False)
         newContact.save()
-    return render(request, "home.html", {'form':ContactForm})
+    projects = Projects.objects.all()
+    return render(request, "home.html", {'form':ContactForm, 'projects': projects})
