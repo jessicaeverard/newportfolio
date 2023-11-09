850a5768c4603c7308d0ae3a9eb987a7809133d1
Jess <jessica@mediatrader.com>
2023-11-09 15:52:00 +0000

    test

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -141,8 +141,17 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
+# STATIC_URL = '/static/'
+# STATIC_ROOT = 'actualwebsite/static'
+
+STATIC_ROOT = os.path.join(BASE_DIR,'static')
 STATIC_URL = '/static/'
-STATIC_ROOT = 'actualwebsite/static'
+
+# STATICFILES_DIRS = [
+#     '/static/',
+# ]
+
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
